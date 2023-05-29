d4317b7b8b5acd449e350415a7e64b35737bf10e
SoraTenshi <dream@neoncity.dev>
2023-05-29 02:12:07 +0200

    Okay fix dependencies to zigwin32

--- build.zig
+++ build.zig
@@ -1,13 +1,14 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    var zigwin32 = b.dependency("zigwin32", .{});
+
     _ = b.addModule(
         "zigwin-console",
         .{
             .source_file = .{ .path = "console.zig" },
-            .dependencies = .{
-                .name = "zigwin32",
-                .module = b.dependency("zigwin32", .{}),
+            .dependencies = &.{
+                .{ .name = "zigwin32", .module = zigwin32.module("zigwin32") },
             },
         },
     );
