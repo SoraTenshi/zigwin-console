47497aaa56a5cde44e6cf0ca8b164fd3f0a643f1
SoraTenshi <dream@neoncity.dev>
2024-08-19 19:32:41 +0200

    update: version

--- build.zig
+++ build.zig
@@ -4,13 +4,13 @@ pub fn build(b: *std.Build) void {
     var zigwin32 = b.dependency("zigwin32", .{});
 
     const zigwin_console = b.addModule("zigwin-console", .{
-        .root_source_file = .{ .path = "console.zig" },
+        .root_source_file = b.path("console.zig"),
     });
     zigwin_console.addImport("zigwin32", zigwin32.module("zigwin32"));
 
     const optimize = b.standardOptimizeOption(.{});
     const zigwin_console_test = b.addTest(.{
-        .root_source_file = .{ .path = "console.zig" },
+        .root_source_file = b.path("console.zig"),
         .target = b.resolveTargetQuery(.{ .os_tag = .windows }),
         .optimize = optimize,
     });
