365b3409cf895ed57f03e2fc6d0ef93b1e5f1fa1
SoraTenshi <dream@neoncity.dev>
2025-03-09 01:42:23 +0100

    Upgrade to zig 0.14

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) void {
     const zigwin_console = b.addModule("zigwin-console", .{
         .root_source_file = b.path("console.zig"),
     });
-    zigwin_console.addImport("zigwin32", zigwin32.module("zigwin32"));
+    zigwin_console.addImport("zigwin32", zigwin32.module("win32"));
 
     const optimize = b.standardOptimizeOption(.{});
     const zigwin_console_test = b.addTest(.{
@@ -14,7 +14,7 @@ pub fn build(b: *std.Build) void {
         .target = b.resolveTargetQuery(.{ .os_tag = .windows }),
         .optimize = optimize,
     });
-    zigwin_console_test.root_module.addImport("zigwin32", zigwin32.module("zigwin32"));
+    zigwin_console_test.root_module.addImport("zigwin32", zigwin32.module("win32"));
 
     const run_lib_tests = b.addRunArtifact(zigwin_console_test);
     const test_step = b.step("test", "Run the library tests");
